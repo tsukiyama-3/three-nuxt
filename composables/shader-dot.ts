3e1051e68e95dbe46c6f9fd70d1c589768cbead3
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2022-07-26 20:21:10 +0900

    add shader

--- composables/shader-dot.ts
+++ composables/shader-dot.ts
@@ -1,11 +1,50 @@
-import { AmbientLight, OrthographicCamera, Scene, WebGLRenderer } from 'three'
+import { AmbientLight, Mesh, OrthographicCamera, PlaneGeometry, Scene, ShaderMaterial, WebGLRenderer } from 'three'
 import { Ref } from 'vue'
+
+// Shader Material
+const mat = new ShaderMaterial({
+  vertexShader:
+    `
+    void main() {
+      vec4 worldPosition = modelMatrix * vec4( position, 1.0 );
+      vec4 mvPosition = viewMatrix * worldPosition;
+      gl_Position = projectionMatrix * mvPosition;
+    }
+    `,
+  fragmentShader:
+    `
+    void main() {
+      gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+    `
+})
 
 // シーン
 const scene = new Scene()
 
 // カメラ
-const camera = new OrthographicCamera(0, 0, 0, 0, 8, 512)
+const camera = new OrthographicCamera(-1, 1, 1, -1, 0, -1)
+
+// ジオメトリ
+const geometry = new PlaneGeometry(2, 2, 10, 10)
+
+// ユニフォーム
+const uniform = {
+  uAspect: {
+    value: 1
+  },
+}
+
+// マテリアル
+const material = new ShaderMaterial({
+  uniforms: uniform,
+  vertexShader: mat.vertexShader,
+  fragmentShader: mat.fragmentShader,
+})
+
+const mesh = new Mesh(geometry, material)
+
+scene.add(mesh)
 
 // ライト
 const light = new AmbientLight(0xffffff, 1.0)
