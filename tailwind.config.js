a783501f4b83bcfb76e927e9ae78981799a6e701
tsukiyama-3 <tsukiyama.mail@gmail.com>
2024-09-03 01:08:29 +0900

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.vue', './components/**/*.vue', './layouts/**/*.vue', './pages/**/*.vue'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
